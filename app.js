e44f76b65b1b3cd5b900dcc61dea8187d93a93d7
Elie Ruvinga <ruvingaelie@gmail.com>
2023-02-08 02:32:49 +0100

    setup Api (test)

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// notre Application EPRESS
+
+const express = require('express');
+const app = express();  // methode express
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+})
+
+app.use((req, res) => {
+
+    const stuff = [
+        {
+            _id: '1234',
+            title: "mon article",
+            description: "description de l'article",
+            imageUrl: "",
+            useId: 25
+        }
+    ]
+    res.json(stuff);
+    res.status(200);
+});
+
+
+
+module.exports = app;
