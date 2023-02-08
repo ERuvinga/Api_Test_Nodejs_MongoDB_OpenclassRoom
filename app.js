180d24274dd8060a54511929d3b01b4ae3027e7f
Elie Ruvinga <ruvingaelie@gmail.com>
2023-02-08 03:16:09 +0100

    adding a shema and model with mongoose

--- app.js
+++ app.js
@@ -4,27 +4,35 @@
 const express = require('express');
 require('./db/db'); //include a function that connect api to data base
 const app = express();  // methode express
+const model = require('./Model/product')
 
+//configuration
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 })
+app.use(express.json());
 
-app.use((req, res) => {
+//routes
+app.post('/api/products',(req, res) => {
+    const product = new model({
+        ...req.body
+    })
 
-    const stuff = [
-        {
-            _id: '1234',
-            title: "mon article",
-            description: "description de l'article",
-            imageUrl: "",
-            useId: 25
-        }
-    ]
-    res.json(stuff);
-    res.status(200);
+    product.save()
+    .then((product)=>{
+        res.json(product);
+        res.status(200);
+        console.log(product);
+    })
+    .catch(error=>{
+        res.json({message: 'product not creat'});
+        res.status(404);
+        console.log(error); 
+    })
+
 });
 
 
