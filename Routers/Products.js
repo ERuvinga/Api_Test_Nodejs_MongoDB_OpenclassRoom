42ec210ce8ca62ab2282020674cb50a7bbcad20a
ERuvinga <ruvingaelie@gmail.com>
2023-02-13 18:49:51 +0200

    Update  getOneProduct controller

--- Routers/Products.js
+++ Routers/Products.js
@@ -11,7 +11,6 @@ router.get('/', productCtrl.getAllProducts);
 
 //find one product with Id
 router.get('/:id', productCtrl.getOneProduct);
-router.options('/:id', productCtrl.getOneProduct);
 
 //update data of One product
 router.put('/:id', productCtrl.updateProduct);
